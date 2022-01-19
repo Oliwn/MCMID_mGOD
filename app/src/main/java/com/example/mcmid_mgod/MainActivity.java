53eb6178f6f7166a05ec1aa4e08eb50157fa8c1a
dorafrau <dora.frauscher@gmx.at>
2022-01-19 13:47:21 +0100

    first draft login

--- app/src/main/java/com/example/mcmid_mgod/MainActivity.java
+++ app/src/main/java/com/example/mcmid_mgod/MainActivity.java
@@ -1,15 +1,27 @@
 package com.example.mcmid_mgod;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.core.graphics.drawable.DrawableCompat;
 
+import android.content.ClipData;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.util.TypedValue;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    boolean currentUser = false;
+    private Menu menu;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -51,5 +63,58 @@ public class MainActivity extends AppCompatActivity {
             // TextView textView = findViewById(R.id.textViewLastRecording);
             // textView.setText(message);
         }
+
+
+
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        this.menu = menu;
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_user, menu);
+
+
+
+        return true;
+    }
+
+    @Override
+    public boolean onMenuOpened(int featureId, Menu menu) {
+        if (!currentUser){ // show LOGIN menu
+            menu.getItem(0).setVisible(true);
+            menu.getItem(1).setVisible(false);
+        }
+        if (currentUser){ // show LOGOUT menu
+            menu.getItem(0).setVisible(true);
+            menu.getItem(1).setVisible(true);
+        }
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){ // LOGIN
+            case R.id.LoginItem:
+
+
+
+                Intent i = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(i);
+                currentUser = true;
+
+
+
+                return true;
+            case R.id.LogoutItem:
+                Toast toastLogout = Toast.makeText(getApplicationContext(), "Youre logged out", Toast.LENGTH_SHORT);
+                toastLogout.show();
+                // do log out user
+
+
+        }
+        return super.onOptionsItemSelected(item);
+
+    }
+
 }
