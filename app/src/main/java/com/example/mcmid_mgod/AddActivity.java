8ed9106d1bac5cbddf4c7ccfd924850d02252257
dorafrau <dora.frauscher@gmx.at>
2022-01-19 22:25:09 +0100

    Login, blablabla

--- app/src/main/java/com/example/mcmid_mgod/AddActivity.java
+++ app/src/main/java/com/example/mcmid_mgod/AddActivity.java
@@ -9,6 +9,8 @@ import android.view.MenuInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 import com.fasterxml.jackson.databind.JsonSerializer;
 
@@ -24,18 +26,37 @@ public class AddActivity extends MainActivity {
             public void onClick(View view) {
                 // Submit Input Data
                 EditText mEditText_BP = findViewById(R.id.editText_BP);
-                int BP = Integer.parseInt(mEditText_BP.getText().toString());
+                if (mEditText_BP.getText() != null){
+                    String BP = mEditText_BP.getText().toString();
+                }
                 mEditText_BP.setInputType(InputType.TYPE_NULL);
 
                 EditText mEditText_weight = findViewById(R.id.editText_weight);
-                int weight = Integer.parseInt(mEditText_BP.getText().toString());
+                if (mEditText_weight.getText() != null){
+                    //int weight = Integer.parseInt(mEditText_BP.getText().toString()); !!!!!!!!!!!CRASHED !!!!!!!!!!!!!!
+                }
+
                 mEditText_weight.setInputType(InputType.TYPE_NULL);
 
                 EditText mEditText_additional = findViewById(R.id.editText_additional);
-                String additional = mEditText_additional.getText().toString();
+                if (mEditText_additional.getText() != null){
+                    String additional = mEditText_additional.getText().toString();
+                }
                 mEditText_additional.setInputType(InputType.TYPE_NULL);
+                menu.getItem(0).setVisible(true);
+                Toast toastLogout = Toast.makeText(getApplicationContext(), "Successfully submitted", Toast.LENGTH_SHORT);
+                mButtonSubmit.setEnabled(false);
             }
         });
+        ImageButton buttonBluetooth = findViewById(R.id.imageButton_bluetooth);
+        buttonBluetooth.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // BLUETOOTH
+            }
+        });
+
+
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
